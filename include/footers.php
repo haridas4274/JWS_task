590fb9851d8b802cfcd334263e71ea38a2316459
Haridas <haridasnamakkal@gmail.com>
2023-10-30 14:00:23 +0530

    setup index pages with layout

---
+++ include/footers.php
@@ -0,0 +1,54 @@
+<footer class="site-footer style-1">
+          
+          <!-- Footer Top -->
+          <div class="footer-top">
+              <div class="container">
+                  <div class="row">
+                      <div class="col-xl-6 col-lg-12 wow fadeInUp" data-wow-delay="0.1s">
+                          <div class="widget widget_about">
+                              <div class="footer-logo logo-white">
+                                  <a href="index.html"><img src="https://jayamwebsolutions.com/images/logo.png" alt=""></a>
+                              </div>
+                              <p class="text">Over the years of experience, we gained reputation in web design services, SEO services, web development and mobile app development services. We take pride in saying that we have completed more than 1700+ projects till date.</p>
+                              <div class="dz-social-icon style-1">
+                                  <ul>
+                                      <li><a href="#" target="_blank"><i
+                                                  class="fa-brands fa-facebook-f"></i></a></li>
+                                      <li><a href="#"
+                                              target="_blank"><i class="fa-brands fa-youtube"></i></a></li>
+                                      <li><a href="#"
+                                              target="_blank"><i class="fa-brands fa-linkedin"></i></a></li>
+                                      <li><a href="#" target="_blank"><i
+                                                  class="fa-brands fa-instagram"></i></a></li>
+                                  </ul>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="col-xl-6 col-lg-12">
+                          <h3>footer Section</h3>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <!-- Footer Top End -->
+
+
+      </footer>
+      <!-- Footer End -->
+
+      <button class="scroltop" type="button"><i class="fas fa-arrow-up"></i></button>
+          </div>
+          <!-- JAVASCRIPT FILES ========================================= -->
+          <script src="assets/js/jquery.min.js"></script><!-- JQUERY MIN JS -->
+          <script src="assets/vendor/wow/wow.min.js"></script><!-- WOW JS -->
+          <script src="assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script><!-- BOOTSTRAP MIN JS -->
+          <script src="assets/vendor/bootstrap-select/dist/js/bootstrap-select.min.js"></script><!-- BOOTSTRAP SELECT MIN JS -->
+          <script src="assets/vendor/counter/waypoints-min.js"></script><!-- WAYPOINTS JS -->
+          <script src="assets/vendor/counter/counterup.min.js"></script><!-- COUNTERUP JS -->
+          <script src="assets/vendor/swiper/swiper-bundle.min.js"></script><!-- SWIPER JS -->
+          <script src="assets/js/dz.carousel.js"></script><!-- DZ CAROUSEL JS -->
+          <script src="assets/js/dz.ajax.js"></script><!-- AJAX -->
+          <script src="assets/js/custom.js"></script><!-- CUSTOM JS -->
+      </body>
+
+</html>
--- index.php
+++ index.php
@@ -1,13 +1,399 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JWS Task Product listing page</title>
-</head>
-<body>
+<?php include('include/headers.php') ?>
+<div class="page-content bg-white">
 
 
-    
-</body>
-</html>
+
+	
+	
+	
+
+	<!-- Book Sale -->
+	<section class="content-inner-1">
+		<div class="container">
+			<div class="section-head book-align">
+				<h2 class="title mb-0">Products List</h2>
+				<div class="pagination-align style-1">
+					<div class="swiper-button-prev"><i class="fa-solid fa-angle-left"></i></div>
+					<div class="swiper-pagination-two"></div>
+					<div class="swiper-button-next"><i class="fa-solid fa-angle-right"></i></div>
+				</div>
+			</div>
+			<div class="swiper-container books-wrapper-3 swiper-four">
+				<div class="swiper-wrapper">
+					<div class="swiper-slide">
+						<div class="books-card style-3 wow fadeInUp" data-wow-delay="0.1s">
+							<div class="dz-media">
+								<img src="assets/images/books/grid/book5.jpg" alt="book">
+							</div>
+							<div class="dz-content">
+								<h5 class="title"><a href="books-grid-view.html">Take Out Tango</a></h5>
+								<ul class="dz-tags">
+									<li><a href="books-grid-view.html">SPORTS,</a></li>
+									<li><a href="books-grid-view.html">DRAMA</a></li>
+								</ul>
+								<div class="book-footer">
+									<div class="rate">
+										<i class="flaticon-star"></i> 6.8
+									</div>
+									<div class="price">
+										<span class="price-num">$9.5</span>
+										<del>$12.0</del>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="swiper-slide">
+						<div class="books-card style-3 wow fadeInUp" data-wow-delay="0.2s">
+							<div class="dz-media">
+								<img src="assets/images/books/grid/book11.jpg" alt="book">
+							</div>
+							<div class="dz-content">
+								<h5 class="title"><a href="books-grid-view.html">The Missadventure</a></h5>
+								<ul class="dz-tags">
+									<li><a href="books-grid-view.html">SPORTS,</a></li>
+									<li><a href="books-grid-view.html">DRAMA</a></li>
+								</ul>
+								<div class="book-footer">
+									<div class="rate">
+										<i class="flaticon-star"></i> 6.8
+									</div>
+									<div class="price">
+										<span class="price-num">$9.5</span>
+										<del>$12.0</del>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="swiper-slide">
+						<div class="books-card style-3 wow fadeInUp" data-wow-delay="0.3s">
+							<div class="dz-media">
+								<img src="assets/images/books/grid/book7.jpg" alt="book">
+							</div>
+							<div class="dz-content">
+								<h5 class="title"><a href="books-grid-view.html">Seconds [PART 1]</a></h5>
+								<ul class="dz-tags">
+									<li><a href="books-grid-view.html">SPORTS,</a></li>
+									<li><a href="books-grid-view.html">DRAMA</a></li>
+								</ul>
+								<div class="book-footer">
+									<div class="rate">
+										<i class="flaticon-star"></i> 6.8
+									</div>
+									<div class="price">
+										<span class="price-num">$9.5</span>
+										<del>$12.0</del>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="swiper-slide">
+						<div class="books-card style-3 wow fadeInUp" data-wow-delay="0.4s">
+							<div class="dz-media">
+								<img src="assets/images/books/grid/book12.jpg" alt="book">
+							</div>
+							<div class="dz-content">
+								<h5 class="title"><a href="books-grid-view.html">The Missadventure</a></h5>
+								<ul class="dz-tags">
+									<li><a href="books-grid-view.html">SPORTS,</a></li>
+									<li><a href="books-grid-view.html">DRAMA</a></li>
+								</ul>
+								<div class="book-footer">
+									<div class="rate">
+										<i class="flaticon-star"></i> 6.8
+									</div>
+									<div class="price">
+										<span class="price-num">$9.5</span>
+										<del>$12.0</del>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="swiper-slide">
+						<div class="books-card style-3 wow fadeInUp" data-wow-delay="0.5s">
+							<div class="dz-media">
+								<img src="assets/images/books/grid/book15.jpg" alt="book">
+							</div>
+							<div class="dz-content">
+								<h5 class="title"><a href="books-grid-view.html">Terrible Madness</a></h5>
+								<ul class="dz-tags">
+									<li><a href="books-grid-view.html">SPORTS,</a></li>
+									<li><a href="books-grid-view.html">DRAMA</a></li>
+								</ul>
+								<div class="book-footer">
+									<div class="rate">
+										<i class="flaticon-star"></i> 6.8
+									</div>
+									<div class="price">
+										<span class="price-num">$9.5</span>
+										<del>$12.0</del>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="swiper-slide">
+						<div class="books-card style-3 wow fadeInUp" data-wow-delay="0.6s">
+							<div class="dz-media">
+								<img src="assets/images/books/grid/book3.jpg" alt="book">
+							</div>
+							<div class="dz-content">
+								<h5 class="title"><a href="books-grid-view.html">Battle Drive</a></h5>
+								<ul class="dz-tags">
+									<li><a href="books-grid-view.html">SPORTS,</a></li>
+									<li><a href="books-grid-view.html">DRAMA</a></li>
+								</ul>
+								<div class="book-footer">
+									<div class="rate">
+										<i class="flaticon-star"></i> 6.8
+									</div>
+									<div class="price">
+										<span class="price-num">$9.5</span>
+										<del>$12.0</del>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Book Sale End -->
+
+	<!-- Feature Product -->
+	<!-- <section class="content-inner-1 bg-grey reccomend">
+		<div class="container">
+			<div class="section-head text-center">
+				<div class="circle style-1"></div>
+				<h2 class="title">Featured Product</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+					labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+					laboris </p>
+			</div>
+		</div>
+		<div class="container">
+			<div class="swiper-container books-wrapper-2 swiper-three">
+				<div class="swiper-wrapper">
+					<div class="swiper-slide">
+						<div class="books-card style-2">
+							<div class="dz-media">
+								<img src="images/books/large/bigbook1.jpg" alt="book">
+							</div>
+							<div class="dz-content">
+								<h6 class="sub-title">BEST SELLER</h6>
+								<h2 class="title">A Heavy LIft</h2>
+								<ul class="dz-tags">
+									<li>Napoleon Hill</li>
+									<li>Business & Strategy</li>
+								</ul>
+								<p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+									eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+									veniam, quis nostrud exercitation ullamco laboris </p>
+								<div class="price">
+									<span class="price-num">$9.5</span>
+									<del>$12.0</del>
+									<span class="badge">20% OFF</span>
+								</div>
+								<div class="bookcard-footer">
+									<a href="shop-cart.html" class="btn btn-primary btnhover m-t15 m-r10">Buy
+										Now</a>
+									<a href="books-detail.html" class="btn btn-outline-secondary btnhover m-t15">See Details</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="swiper-slide">
+						<div class="books-card style-2">
+							<div class="dz-media">
+								<img src="images/books/large/bigbook2.jpg" alt="book">
+							</div>
+							<div class="dz-content">
+								<h6 class="sub-title">BEST SELLER</h6>
+								<h2 class="title">A Heavy LIft</h2>
+								<ul class="dz-tags">
+									<li>Napoleon Hill</li>
+									<li>Business & Strategy</li>
+								</ul>
+								<p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+									eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+									veniam, quis nostrud exercitation ullamco laboris </p>
+								<div class="price">
+									<span class="price-num">$9.5</span>
+									<del>$12.0</del>
+									<span class="badge">20% OFF</span>
+								</div>
+								<div class="bookcard-footer">
+									<a href="shop-cart.html" class="btn btn-primary btnhover m-t15 m-r10">Buy
+										Now</a>
+									<a href="books-detail.html" class="btn btn-outline-secondary btnhover m-t15">See Details</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="swiper-slide">
+						<div class="books-card style-2">
+							<div class="dz-media">
+								<img src="images/books/large/bigbook1.jpg" alt="book">
+							</div>
+							<div class="dz-content">
+								<h6 class="sub-title">BEST SELLER</h6>
+								<h2 class="title">A Heavy LIft</h2>
+								<ul class="dz-tags">
+									<li>Napoleon Hill</li>
+									<li>Business & Strategy</li>
+								</ul>
+								<p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+									eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+									veniam, quis nostrud exercitation ullamco laboris </p>
+								<div class="price">
+									<span class="price-num">$9.5</span>
+									<del>$12.0</del>
+									<span class="badge">20% OFF</span>
+								</div>
+								<div class="bookcard-footer">
+									<a href="shop-cart.html" class="btn btn-primary btnhover m-t15 m-r10">Buy
+										Now</a>
+									<a href="books-detail.html" class="btn btn-outline-secondary btnhover m-t15">See Details</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="swiper-slide">
+						<div class="books-card style-2">
+							<div class="dz-media">
+								<img src="images/books/large/bigbook2.jpg" alt="book">
+							</div>
+							<div class="dz-content">
+								<h6 class="sub-title">BEST SELLER</h6>
+								<h2 class="title">A Heavy LIft</h2>
+								<ul class="dz-tags">
+									<li>Napoleon Hill</li>
+									<li>Business & Strategy</li>
+								</ul>
+								<p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+									eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+									veniam, quis nostrud exercitation ullamco laboris </p>
+								<div class="price">
+									<span class="price-num">$9.5</span>
+									<del>$12.0</del>
+									<span class="badge">20% OFF</span>
+								</div>
+								<div class="bookcard-footer">
+									<a href="shop-cart.html" class="btn btn-primary btnhover m-t15 m-r10">Buy
+										Now</a>
+									<a href="books-detail.html" class="btn btn-outline-secondary btnhover m-t15">See Details</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="swiper-slide">
+						<div class="books-card style-2">
+							<div class="dz-media">
+								<img src="images/books/large/bigbook1.jpg" alt="book">
+							</div>
+							<div class="dz-content">
+								<h6 class="sub-title">BEST SELLER</h6>
+								<h2 class="title">A Heavy LIft</h2>
+								<ul class="dz-tags">
+									<li>Napoleon Hill</li>
+									<li>Business & Strategy</li>
+								</ul>
+								<p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+									eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+									veniam, quis nostrud exercitation ullamco laboris </p>
+								<div class="price">
+									<span class="price-num">$9.5</span>
+									<del>$12.0</del>
+									<span class="badge">20% OFF</span>
+								</div>
+								<div class="bookcard-footer">
+									<a href="shop-cart.html" class="btn btn-primary btnhover m-t15 m-r10">Buy
+										Now</a>
+									<a href="books-detail.html" class="btn btn-outline-secondary btnhover m-t15">See Details</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="swiper-slide">
+						<div class="books-card style-2">
+							<div class="dz-media">
+								<img src="images/books/large/bigbook2.jpg" alt="book">
+							</div>
+							<div class="dz-content">
+								<h6 class="sub-title">BEST SELLER</h6>
+								<h2 class="title">A Heavy LIft</h2>
+								<ul class="dz-tags">
+									<li>Napoleon Hill</li>
+									<li>Business & Strategy</li>
+								</ul>
+								<p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+									eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+									veniam, quis nostrud exercitation ullamco laboris </p>
+								<div class="price">
+									<span class="price-num">$9.5</span>
+									<del>$12.0</del>
+									<span class="badge">20% OFF</span>
+								</div>
+								<div class="bookcard-footer">
+									<a href="shop-cart.html" class="btn btn-primary btnhover m-t15 m-r10">Buy
+										Now</a>
+									<a href="books-detail.html" class="btn btn-outline-secondary btnhover m-t15">See Details</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="pagination-align style-2">
+					<div class="swiper-button-prev"><i class="fa-solid fa-angle-left"></i></div>
+					<div class="swiper-pagination-three"></div>
+					<div class="swiper-button-next"><i class="fa-solid fa-angle-right"></i></div>
+				</div>
+			</div>
+		</div>
+	</section> -->
+	<!-- Feature Product End -->
+
+
+
+
+	<!-- Newsletter -->
+	<section class="py-5 newsletter-wrapper" style="background-image: url('assets/images/background/bg1.jpg'); background-size: cover;">
+		<div class="container">
+			<div class="subscride-inner">
+				<div class="row style-1 justify-content-xl-between justify-content-lg-center align-items-center text-xl-start text-center">
+					<div class="col-xl-7 col-lg-12 wow fadeInUp" data-wow-delay="0.1s">
+						<div class="section-head mb-0">
+							<h2 class="title text-white my-lg-3 mt-0">Subscribe our newsletter for newest books
+								updates</h2>
+						</div>
+					</div>
+					<div class="col-xl-5 col-lg-6 wow fadeInUp" data-wow-delay="0.2s">
+						<form class="dzSubscribe style-1" action="https://bookland.dexignzone.com/xhtml/script/mailchamp.php" method="post">
+							<div class="dzSubscribeMsg"></div>
+							<div class="form-group">
+								<div class="input-group mb-0">
+									<input name="dzEmail" required="required" type="email" class="form-control bg-transparent text-white" placeholder="Your Email Address">
+									<div class="input-group-addon">
+										<button name="submit" value="Submit" type="submit" class="btn btn-primary btnhover">
+											<span>SUBSCRIBE</span>
+											<i class="fa-solid fa-paper-plane"></i>
+										</button>
+									</div>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Newsletter End -->
+
+</div>
+
+<!-- Footer -->
+<?php include('include/footers.php') ?>
